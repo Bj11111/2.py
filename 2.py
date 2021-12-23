d24853847e63cafc6c3a8b4180205abc48f17a05
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2021-12-24 07:53:49 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,210 +1,191 @@
-from tkinter import *
 import numpy as np
+from math import inf as infinity
+import itertools
+import random
+import time
 
-size_of_board = 600
-symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
-symbol_thickness = 50
-symbol_X_color = '#EE4035'
-symbol_O_color = '#0492CF'
-Green_color = '#7BC043'
+game_state = [[' ',' ',' '],
+              [' ',' ',' '],
+              [' ',' ',' ']]
+players = ['X','O']
 
+def play_move(state, player, block_num):
+    if state[int((block_num-1)/3)][(block_num-1)%3] is ' ':
+        state[int((block_num-1)/3)][(block_num-1)%3] = player
+    else:
+        block_num = int(input("Block is not empty, ya blockhead! Choose again: "))
+        play_move(state, player, block_num)
+    
+def copy_game_state(state):
+    new_state = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+    for i in range(3):
+        for j in range(3):
+            new_state[i][j] = state[i][j]
+    return new_state
+    
+def check_current_state(game_state):    
+    # Check horizontals
+    if (game_state[0][0] == game_state[0][1] and game_state[0][1] == game_state[0][2] and game_state[0][0] is not ' '):
+        return game_state[0][0], "Done"
+    if (game_state[1][0] == game_state[1][1] and game_state[1][1] == game_state[1][2] and game_state[1][0] is not ' '):
+        return game_state[1][0], "Done"
+    if (game_state[2][0] == game_state[2][1] and game_state[2][1] == game_state[2][2] and game_state[2][0] is not ' '):
+        return game_state[2][0], "Done"
+    
+    # Check verticals
+    if (game_state[0][0] == game_state[1][0] and game_state[1][0] == game_state[2][0] and game_state[0][0] is not ' '):
+        return game_state[0][0], "Done"
+    if (game_state[0][1] == game_state[1][1] and game_state[1][1] == game_state[2][1] and game_state[0][1] is not ' '):
+        return game_state[0][1], "Done"
+    if (game_state[0][2] == game_state[1][2] and game_state[1][2] == game_state[2][2] and game_state[0][2] is not ' '):
+        return game_state[0][2], "Done"
+    
+    # Check diagonals
+    if (game_state[0][0] == game_state[1][1] and game_state[1][1] == game_state[2][2] and game_state[0][0] is not ' '):
+        return game_state[1][1], "Done"
+    if (game_state[2][0] == game_state[1][1] and game_state[1][1] == game_state[0][2] and game_state[2][0] is not ' '):
+        return game_state[1][1], "Done"
+    
+    # Check if draw
+    draw_flag = 0
+    for i in range(3):
+        for j in range(3):
+            if game_state[i][j] is ' ':
+                draw_flag = 1
+    if draw_flag is 0:
+        return None, "Draw"
+    
+    return None, "Not Done"
 
-class Tic_Tac_Toe():
-    # ------------------------------------------------------------------
-    # Initialization Functions:
-    # ------------------------------------------------------------------
-    def __init__(self):
-        self.window = Tk()
-        self.window.title('Tic-Tac-Toe')
-        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
-        self.canvas.pack()
-        # Input from user in form of clicks
-        self.window.bind('<Button-1>', self.click)
+def print_board(game_state):
+    print('----------------')
+    print('| ' + str(game_state[0][0]) + ' || ' + str(game_state[0][1]) + ' || ' + str(game_state[0][2]) + ' |')
+    print('----------------')
+    print('| ' + str(game_state[1][0]) + ' || ' + str(game_state[1][1]) + ' || ' + str(game_state[1][2]) + ' |')
+    print('----------------')
+    print('| ' + str(game_state[2][0]) + ' || ' + str(game_state[2][1]) + ' || ' + str(game_state[2][2]) + ' |')
+    print('----------------')
+    
+  
+# Initialize state values
+player = ['X','O',' ']
+states_dict = {}
+all_possible_states = [[list(i[0:3]),list(i[3:6]),list(i[6:10])] for i in itertools.product(player, repeat = 9)]
+n_states = len(all_possible_states) # 2 players, 9 spaces
+n_actions = 9   # 9 spaces
+state_values_for_AI_O = np.full((n_states),0.0)
+state_values_for_AI_X = np.full((n_states),0.0)
+print("n_states = %i \nn_actions = %i"%(n_states, n_actions))
 
-        self.initialize_board()
-        self.player_X_turns = True
-        self.board_status = np.zeros(shape=(3, 3))
+# State values for AI 'O'
+for i in range(n_states):
+    states_dict[i] = all_possible_states[i]
+    winner, _ = check_current_state(states_dict[i])
+    if winner == 'O':   # AI won
+        state_values_for_AI_O[i] = 1
+    elif winner == 'X':   # AI lost
+        state_values_for_AI_O[i] = -1
+        
+# State values for AI 'X'       
+for i in range(n_states):
+    winner, _ = check_current_state(states_dict[i])
+    if winner == 'O':   # AI lost
+        state_values_for_AI_X[i] = -1
+    elif winner == 'X':   # AI won
+        state_values_for_AI_X[i] = 1
 
-        self.player_X_starts = True
-        self.reset_board = False
-        self.gameover = False
-        self.tie = False
-        self.X_wins = False
-        self.O_wins = False
+def update_state_value_O(curr_state_idx, next_state_idx, learning_rate):
+    new_value = state_values_for_AI_O[curr_state_idx] + learning_rate*(state_values_for_AI_O[next_state_idx]  - state_values_for_AI_O[curr_state_idx])
+    state_values_for_AI_O[curr_state_idx] = new_value
+    
+def update_state_value_X(curr_state_idx, next_state_idx, learning_rate):
+    new_value = state_values_for_AI_X[curr_state_idx] + learning_rate*(state_values_for_AI_X[next_state_idx]  - state_values_for_AI_X[curr_state_idx])
+    state_values_for_AI_X[curr_state_idx] = new_value
 
-        self.X_score = 0
-        self.O_score = 0
-        self.tie_score = 0
-
-    def mainloop(self):
-        self.window.mainloop()
-
-    def initialize_board(self):
-        for i in range(2):
-            self.canvas.create_line((i + 1) * size_of_board / 3, 0, (i + 1) * size_of_board / 3, size_of_board)
-
-        for i in range(2):
-            self.canvas.create_line(0, (i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
-
-    def play_again(self):
-        self.initialize_board()
-        self.player_X_starts = not self.player_X_starts
-        self.player_X_turns = self.player_X_starts
-        self.board_status = np.zeros(shape=(3, 3))
-
-    # ------------------------------------------------------------------
-    # Drawing Functions:
-    # The modules required to draw required game based object on canvas
-    # ------------------------------------------------------------------
-
-    def draw_O(self, logical_position):
-        logical_position = np.array(logical_position)
-        # logical_position = grid value on the board
-        # grid_position = actual pixel values of the center of the grid
-        grid_position = self.convert_logical_to_grid_position(logical_position)
-        self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
-                                grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
-                                outline=symbol_O_color)
-
-    def draw_X(self, logical_position):
-        grid_position = self.convert_logical_to_grid_position(logical_position)
-        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
-                                grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
-                                fill=symbol_X_color)
-        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] + symbol_size,
-                                grid_position[0] + symbol_size, grid_position[1] - symbol_size, width=symbol_thickness,
-                                fill=symbol_X_color)
-
-    def display_gameover(self):
-
-        if self.X_wins:
-            self.X_score += 1
-            text = 'Winner: Player 1 (X)'
-            color = symbol_X_color
-        elif self.O_wins:
-            self.O_score += 1
-            text = 'Winner: Player 2 (O)'
-            color = symbol_O_color
+def getBestMove(state, player, epsilon):
+    '''
+    Reinforcement Learning Algorithm
+    '''    
+    moves = []
+    curr_state_values = []
+    empty_cells = []
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] is ' ':
+                empty_cells.append(i*3 + (j+1))
+    
+    for empty_cell in empty_cells:
+        moves.append(empty_cell)
+        new_state = copy_game_state(state)
+        play_move(new_state, player, empty_cell)
+        next_state_idx = list(states_dict.keys())[list(states_dict.values()).index(new_state)]
+        if player == 'X':
+            curr_state_values.append(state_values_for_AI_X[next_state_idx])
         else:
-            self.tie_score += 1
-            text = 'Its a tie'
-            color = 'gray'
+            curr_state_values.append(state_values_for_AI_O[next_state_idx])
+        
+    print('Possible moves = ' + str(moves))
+    print('Move values = ' + str(curr_state_values))    
+    best_move_idx = np.argmax(curr_state_values)
+    
+    if np.random.uniform(0,1) <= epsilon:       # Exploration
+        best_move = random.choice(empty_cells)
+        print('Agent decides to explore! Takes action = ' + str(best_move))
+        epsilon *= 0.99
+    else:   #Exploitation
+        best_move = moves[best_move_idx]
+        print('Agent decides to exploit! Takes action = ' + str(best_move))
+    return best_move
 
-        self.canvas.delete("all")
-        self.canvas.create_text(size_of_board / 2, size_of_board / 3, font="cmr 60 bold", fill=color, text=text)
+# PLaying
 
-        score_text = 'Scores \n'
-        self.canvas.create_text(size_of_board / 2, 5 * size_of_board / 8, font="cmr 40 bold", fill=Green_color,
-                                text=score_text)
+#LOAD TRAINED STATE VALUES
+state_values_for_AI_X = np.loadtxt('trained_state_values_X.txt', dtype=np.float64)
+state_values_for_AI_O = np.loadtxt('trained_state_values_O.txt', dtype=np.float64)
 
-        score_text = 'Player 1 (X) : ' + str(self.X_score) + '\n'
-        score_text += 'Player 2 (O): ' + str(self.O_score) + '\n'
-        score_text += 'Tie                    : ' + str(self.tie_score)
-        self.canvas.create_text(size_of_board / 2, 3 * size_of_board / 4, font="cmr 30 bold", fill=Green_color,
-                                text=score_text)
-        self.reset_board = True
-
-        score_text = 'Click to play again \n'
-        self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font="cmr 20 bold", fill="gray",
-                                text=score_text)
-
-    # ------------------------------------------------------------------
-    # Logical Functions:
-    # The modules required to carry out game logic
-    # ------------------------------------------------------------------
-
-    def convert_logical_to_grid_position(self, logical_position):
-        logical_position = np.array(logical_position, dtype=int)
-        return (size_of_board / 3) * logical_position + size_of_board / 6
-
-    def convert_grid_to_logical_position(self, grid_position):
-        grid_position = np.array(grid_position)
-        return np.array(grid_position // (size_of_board / 3), dtype=int)
-
-    def is_grid_occupied(self, logical_position):
-        if self.board_status[logical_position[0]][logical_position[1]] == 0:
-            return False
+learning_rate = 0.2
+epsilon = 0.2
+num_iterations = 10000
+for iteration in range(num_iterations):
+    game_state = [[' ',' ',' '],
+              [' ',' ',' '],
+              [' ',' ',' ']]
+    current_state = "Not Done"
+    print("\nIteration " + str(iteration) + "!")
+    print_board(game_state)
+    winner = None
+    current_player_idx = random.choice([0,1])
+        
+    while current_state == "Not Done":
+        curr_state_idx = list(states_dict.keys())[list(states_dict.values()).index(game_state)]
+        if current_player_idx == 0:     # AI_X's turn
+            print("\nAI X's turn!")         
+            block_choice = getBestMove(game_state, players[current_player_idx], epsilon)
+            play_move(game_state ,players[current_player_idx], block_choice)
+            new_state_idx = list(states_dict.keys())[list(states_dict.values()).index(game_state)]
+            
+        else:       # AI_O's turn
+            print("\nAI O's turn!")   
+            block_choice = getBestMove(game_state, players[current_player_idx], epsilon)
+            play_move(game_state ,players[current_player_idx], block_choice)
+            new_state_idx = list(states_dict.keys())[list(states_dict.values()).index(game_state)]
+        
+        print_board(game_state)
+        #print('State value = ' + str(state_values_for_AI[new_state_idx]))
+        update_state_value_O(curr_state_idx, new_state_idx, learning_rate)
+        update_state_value_X(curr_state_idx, new_state_idx, learning_rate)
+        winner, current_state = check_current_state(game_state)
+        if winner is not None:
+            print(str(winner) + " won!")
         else:
-            return True
+            current_player_idx = (current_player_idx + 1)%2
+        
+        if current_state is "Draw":
+            print("Draw!")
+            
+        #time.sleep(1)
+print('Training Complete!')    
 
-    def is_winner(self, player):
-
-        player = -1 if player == 'X' else 1
-
-        # Three in a row
-        for i in range(3):
-            if self.board_status[i][0] == self.board_status[i][1] == self.board_status[i][2] == player:
-                return True
-            if self.board_status[0][i] == self.board_status[1][i] == self.board_status[2][i] == player:
-                return True
-
-        # Diagonals
-        if self.board_status[0][0] == self.board_status[1][1] == self.board_status[2][2] == player:
-            return True
-
-        if self.board_status[0][2] == self.board_status[1][1] == self.board_status[2][0] == player:
-            return True
-
-        return False
-
-    def is_tie(self):
-
-        r, c = np.where(self.board_status == 0)
-        tie = False
-        if len(r) == 0:
-            tie = True
-
-        return tie
-
-    def is_gameover(self):
-        # Either someone wins or all grid occupied
-        self.X_wins = self.is_winner('X')
-        if not self.X_wins:
-            self.O_wins = self.is_winner('O')
-
-        if not self.O_wins:
-            self.tie = self.is_tie()
-
-        gameover = self.X_wins or self.O_wins or self.tie
-
-        if self.X_wins:
-            print('X wins')
-        if self.O_wins:
-            print('O wins')
-        if self.tie:
-            print('Its a tie')
-
-        return gameover
-
-
-
-
-
-    def click(self, event):
-        grid_position = [event.x, event.y]
-        logical_position = self.convert_grid_to_logical_position(grid_position)
-
-        if not self.reset_board:
-            if self.player_X_turns:
-                if not self.is_grid_occupied(logical_position):
-                    self.draw_X(logical_position)
-                    self.board_status[logical_position[0]][logical_position[1]] = -1
-                    self.player_X_turns = not self.player_X_turns
-            else:
-                if not self.is_grid_occupied(logical_position):
-                    self.draw_O(logical_position)
-                    self.board_status[logical_position[0]][logical_position[1]] = 1
-                    self.player_X_turns = not self.player_X_turns
-
-            # Check if game is concluded
-            if self.is_gameover():
-                self.display_gameover()
-                # print('Done')
-        else:  # Play Again
-            self.canvas.delete("all")
-            self.play_again()
-            self.reset_board = False
-
-
-game_instance = Tic_Tac_Toe()
-game_instance.mainloop()
+# Save state values for future use
+np.savetxt('trained_state_values_X.txt', state_values_for_AI_X, fmt = '%.6f')
+np.savetxt('trained_state_values_O.txt', state_values_for_AI_O, fmt = '%.6f')
