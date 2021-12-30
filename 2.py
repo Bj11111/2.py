5bc0faa429d4ade0276168c6a18e8e89281a7b1e
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2021-12-30 19:56:30 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -19,7 +19,7 @@ def drawBoard(board):
     st.write('    |    |')
     st.write(' ' + board[1] + '  | ' + board[2] + '  | ' + board[3])
     st.write('    |    |')
-
+st.session
 def inputPlayerLetter():
     # Lets the player type which letter they want to be.
     # Returns a list with the player’s letter as the first item, and the computer's letter as the second
@@ -28,12 +28,11 @@ def inputPlayerLetter():
         st.write('Do you want to be X or O?')
         letter = st.text_input('please input X or O?' , 'X O')
         letter = letter.upper()
-st.session
     # the first element in the list is the player’s letter, the second is the computer's letter.
-if letter == 'X':
-    return ['X', 'O']
-else:
-    return ['O', 'X']
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
 
 def whoGoesFirst():
     # Randomly choose the player who goes first.
